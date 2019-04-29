882a7e042285dc2d39757292c11416914945fa9f
ethan0353 <yinziyu7@gmail.com>
2019-04-29 17:47:17 +0800

    add app download

--- src/main/java/com/anbang/p2p/web/controller/UserAuthController.java
+++ src/main/java/com/anbang/p2p/web/controller/UserAuthController.java
@@ -1,5 +1,6 @@
 package com.anbang.p2p.web.controller;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -25,6 +26,7 @@ import com.anbang.p2p.web.vo.CommonVO;
 import com.google.gson.Gson;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 授权验证
@@ -230,5 +232,62 @@ public class UserAuthController {
 			vo.setMsg(e.getClass().getName());
 			return vo;
 		}
+	}
+
+
+	/**
+	 * app 下载
+	 */
+	@RequestMapping("/app")
+	@ResponseBody
+	public String app(int type, HttpServletResponse response) {
+		String fileName;
+		if (type == 1) {
+			fileName="kuailu.apk";
+		} else {
+			fileName="kuailu.ipa";
+		}
+
+		if (fileName != null) {
+			//设置文件路径
+			File file = new File("/data/" + fileName);
+			//File file = new File(realPath , fileName);
+			if (file.exists()) {
+				response.setContentType("application/force-download");// 设置强制下载不打开
+				response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+				byte[] buffer = new byte[1024];
+				FileInputStream fis = null;
+				BufferedInputStream bis = null;
+				try {
+					fis = new FileInputStream(file);
+					bis = new BufferedInputStream(fis);
+					OutputStream os = response.getOutputStream();
+					int i = bis.read(buffer);
+					while (i != -1) {
+						os.write(buffer, 0, i);
+						i = bis.read(buffer);
+					}
+					return "下载成功";
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					if (bis != null) {
+						try {
+							bis.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+					if (fis != null) {
+						try {
+							fis.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+		return "下载失败";
 	}
 }

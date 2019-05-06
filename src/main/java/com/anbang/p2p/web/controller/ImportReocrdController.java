9240db89f8efc0687b1cbc1ac583b134bee09e4e
ethan0353 <yinziyu7@gmail.com>
2019-05-06 17:54:08 +0800

    add template

--- src/main/java/com/anbang/p2p/web/controller/ImportReocrdController.java
+++ src/main/java/com/anbang/p2p/web/controller/ImportReocrdController.java
@@ -23,12 +23,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PushbackInputStream;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.List;
 
 @CrossOrigin
@@ -138,5 +137,55 @@ public class ImportReocrdController {
             e.printStackTrace();
         }
         return CommonVOUtil.error("上传错误，请检查文件格式是否正确");
+    }
+
+    /**
+     * 下载模板
+     */
+    @RequestMapping("/getTemplate")
+    public String getTemplate(HttpServletResponse response) {
+
+        String fileName = "催收销账.xlsx";//被下载文件的名称// 设置文件名，根据业务需要替换成要下载的文件名
+        if (fileName != null) {
+            //设置文件路径
+            String realPath = "/data/files/";
+            File file = new File(realPath , fileName);
+            if (file.exists()) {
+                response.setContentType("application/force-download");// 设置强制下载不打开
+                response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    System.out.println("success");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
